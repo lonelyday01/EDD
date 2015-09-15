5f8a4ca0466104a6a0f2fac8f4ed6f75aa31e61d
lonelyday01 <janixiox@gmail.com>
2015-09-15 13:45:57 -0500

    agregar funciones faltantes

--- calculadora/funciones.c
+++ calculadora/funciones.c
@@ -1,7 +1,18 @@
 #include "funciones.h"
 
-float suma(float num1, float num2)
+float suma(float num1, float num2) 
 {
-	return num1+num2;
+	return  num1 + num2;
 }
-
+float resta(float num1, float num2)
+{
+	return num1-num2;
+}
+float multi(float num1, float num2)
+{
+	return num1*num2;
+}
+float divi(float num1, float num2)
+{
+	return num1/num2;
+}
--- calculadora/funciones.h
+++ calculadora/funciones.h
@@ -1,3 +1,10 @@
 #ifndef FUNCIONES_H
 #define FUNCIONES_H
 
+float suma (float, float);
+float resta(float, float);
+float multi(float,float);
+float divi(float,float);
+
+
+#endif
